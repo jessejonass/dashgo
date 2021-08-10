4469ecb87618340313735348ef421d711414db82
Jessé Jonas <jessejonas13@gmail.com>
2021-08-10 16:08:53 -0300

    feat: started project with next.js and chakra ui

---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+// TODO - Custom Theme Settings
+import { extendTheme, Theme } from '@chakra-ui/react';
+
+export const theme = extendTheme<Theme>({
+  colors: {
+    gray: {
+      "900": "#181823",
+      "800": "#1F2B29",
+      "700": "#353646",
+      "600": "#4B4D63",
+      "500": "#616480",
+      "400": "#797D9A",
+      "300": "#9699B0",
+      "200": "#B3B5C6",
+      "100": "#D1D2DC",
+      "50": "#EEEEF2"
+    }
+  },
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'gray.50',
+      }
+    }
+  }
+});
