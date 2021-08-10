a9a057de1537bde47eea9960611fcac1c21aa345
Jessé Jonas <jessejonas13@gmail.com>
2021-08-10 16:46:21 -0300

    feat: added signin page

---
+++ src/components/pages/SignIn/SignIn.tsx
@@ -0,0 +1,58 @@
+import { FC } from "react";
+import { Flex, Input, Button, FormLabel, FormControl, Stack } from "@chakra-ui/react";
+
+const SignIn: FC = () => {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        flexDir="column"
+        as="form"
+        w="100%"
+        maxW="360"
+        bg="gray.800"
+        p="8"
+        borderRadius="8"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bg="gray.900"
+              variant="filled"
+              _hover={{
+                bg: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="email">Senha</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bg="gray.900"
+              variant="filled"
+              _hover={{
+                bg: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default SignIn;
