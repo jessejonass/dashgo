8292c31d9f881332a95f1c80a9e59078973f9880
Jessé Jonas <jessejonas13@gmail.com>
2021-08-11 14:31:24 -0300

    feat: added sidebar context

---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,34 @@
+import { FC, createContext, useContext, ReactNode } from "react";
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+type SidebarDrawerProviderProps = {
+  children: ReactNode;
+};
+
+// type returned values from context
+type SideBarDrawerContextData = UseDisclosureReturn;
+
+const SidebarDrawerContext = createContext({} as SideBarDrawerContextData);
+
+const SidebarDrawerProvider: FC<SidebarDrawerProviderProps> = ({
+  children,
+}) => {
+  const disclosure = useDisclosure();
+  const router = useRouter();
+
+  // close modal when change router
+  useEffect(() => {
+    disclosure.onClose();
+  }, [router.asPath, disclosure]);
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  );
+};
+
+export default SidebarDrawerProvider;
+export const useSideBarDrawer = () => useContext(SidebarDrawerContext);
