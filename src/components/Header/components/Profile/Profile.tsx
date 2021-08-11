3eeeac45e6b109c95bc9751d0ef5120102066dc6
Jessé Jonas <jessejonas13@gmail.com>
2021-08-11 14:00:58 -0300

    styled: added responsivity in header component using chakra userBreakpointValue

--- src/components/Header/components/Profile/Profile.tsx
+++ src/components/Header/components/Profile/Profile.tsx
@@ -1,15 +1,19 @@
 import { FC } from "react";
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-const Profile: FC = () => {
+import { ProfileProps } from "./types";
+
+const Profile: FC<ProfileProps> = ({ showProfileData = true }) => {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Jessé Jonas</Text>
-        <Text color="gray.300" fontSize="small">
-          jessejonas13@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Jessé Jonas</Text>
+          <Text color="gray.300" fontSize="small">
+            jessejonas13@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
