e55f157ddc3f9093e4f963f2c5cada01bf091945
Jessé Jonas <jessejonas13@gmail.com>
2021-08-11 14:54:59 -0300

    style: added responsivity to pagination component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,21 +1,27 @@
 import { FC } from "react";
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
 import Item from "./components/Item";
 
 const Pagination: FC = () => {
   return (
-    <HStack mt="8" justify="space-between" align="center" spacing="6">
+    <Stack
+      direction={["column", "row"]}
+      mt="8"
+      justify="space-between"
+      align="center"
+      spacing="6"
+    >
       <Box>
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
 
-      <HStack spacing="2">
+      <Stack direction="row" spacing="2">
         <Item isCurrent number={1} />
         <Item number={2} />
         <Item number={3} />
         <Item number={4} />
-      </HStack>
-    </HStack>
+      </Stack>
+    </Stack>
   );
 };
 
