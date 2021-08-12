6a0b893cb3cc14034457a1fca35b578d960ccfc9
Jessé Jonas <jessejonas13@gmail.com>
2021-08-12 11:08:23 -0300

    feat: added react-hook-form and yup to signin

--- src/components/pages/SignIn/SignIn.tsx
+++ src/components/pages/SignIn/SignIn.tsx
@@ -1,8 +1,23 @@
 import { FC } from "react";
 import { Flex, Button, Stack } from "@chakra-ui/react";
-import Input from 'components/Form/Input';
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SignInFormData } from "./types";
+import { signInFormSchema } from './validation';
+
+import Input from "components/Form/Input";
 
 const SignIn: FC = () => {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+  const { errors, isSubmitting } = formState;
+
+  const handleSignin: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -13,13 +28,32 @@ const SignIn: FC = () => {
         bg="gray.800"
         p="8"
         borderRadius="8"
+        onSubmit={handleSubmit(handleSignin)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="email" type="password" label="Password" />
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            error={errors.email}
+            {...register("email")}
+          />
+          <Input
+            name="email"
+            type="password"
+            label="Password"
+            error={errors.password}
+            {...register("password")}
+          />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
